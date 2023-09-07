9a645ee4e42079f2591c5018f5d878ab1c8041e2
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-09-07 22:02:21 +0900

    constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 void main() {
   runApp(const TikTokApp());
 }
-
 class TikTokApp extends StatelessWidget {
   const TikTokApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +14,16 @@ class TikTokApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xFFE9435A),
       ),
-      home: Container(),
+      home: Padding(
+        padding: const EdgeInsets.all(Sizes.size14),
+        child: Container(
+          child: Row(children: const [
+            Text('hello'),
+            Gaps.h20,
+            Text('hello'),
+          ]),
+        ),
+      ),
     );
   }
 }
